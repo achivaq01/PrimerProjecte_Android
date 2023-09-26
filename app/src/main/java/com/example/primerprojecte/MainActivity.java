35fcc46b9724eb6e0dd970d91090afd6027e16e9
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2023-09-26 16:07:53 +0200

    commit n1

---
+++ app/src/main/java/com/example/primerprojecte/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.primerprojecte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.supabutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Nothing happens", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
